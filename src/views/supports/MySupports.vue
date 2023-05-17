8cd2fa3c94bedfc55ce9403b02f9bd2710ba200a
JplsMura <joaopedrolima.s@hotmail.com>
2023-05-17 17:41:54 -0300

    Structuring and componentization of the Supports screen, from the student himself.

--- src/views/supports/MySupports.vue
+++ src/views/supports/MySupports.vue
@@ -1,13 +1,55 @@
 <template>
-    <div class="my-supports">
-      Minhas dúvidas
+  <div>
+    <div class="pageTitle">
+      <span class="title">Minhas Dúvidas</span>
+      <span class="dots">
+        <span></span>
+        <span></span>
+        <span></span>
+      </span>
     </div>
-  </template>
-  
-  <script>
-  
-  export default {
-    name: 'MySupports',
+
+    <div class="content">
+      <div class="container">
+
+        <div class="left">
+          <div class="card">
+            <div class="title bg-laravel">
+              <span class="text">Filtros</span>
+            </div>
+            <div class="modules">
+              <ul class="classes">
+                <li :class="{active : status === ''}" @click="getMySupportsWithStatus('')">Todos</li>
+                <li :class="{active : status === 'A'}" @click="getMySupportsWithStatus('A')">Aguardando Minha Resposta</li>
+                <li :class="{active : status === 'P'}" @click="getMySupportsWithStatus('P')">Aguardando Professor</li>
+                <li :class="{active : status === 'C'}" @click="getMySupportsWithStatus('C')">Finalizados</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div class="right">
+          <div class="content">
+            <div class="comments">
+              <supports />
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Supports from '@/components/Supports.vue'
+
+export default {
+  name: "MySupports",
+
+  components: {
+    Supports
   }
-  </script>
+};
+</script>
   
