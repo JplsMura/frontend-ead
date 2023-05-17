02df65520f2c2f744a712239d67797b2c384ccd1
JplsMura <joaopedrolima.s@hotmail.com>
2023-05-17 16:51:17 -0300

    Parent component subdivision Modules and sub-components, module listing, video player and modules page supports.

---
+++ src/views/modules/ModulesAndLessons.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <div class="pageTitle">
+      <span class="title">CURSO XYZ</span>
+      <span class="dots">
+        <span></span>
+        <span></span>
+        <span></span>
+      </span>
+    </div>
+
+    <div class="content">
+      <div class="container">
+        
+        <modules/>
+
+        <div class="right">
+          <div class="content">
+            
+            <player/>
+
+            <supports-lesson/>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Modules from './components/ListsModules.vue'
+import Player from './components/PlayerVideo.vue'
+import SupportsLesson from './components/Supports.vue'
+
+export default {
+  name: "ModulesAndLessons",
+
+  components: {
+    Modules,
+    Player,
+    SupportsLesson
+  }
+};
+</script>
---
+++ src/views/modules/components/Supports.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="comments">
+    <div class="header">
+      <span class="title">Dúvidas</span>
+      <button class="btn primary">
+        <i class="fas fa-plus"></i>
+        Enviar nova dúvida
+      </button>
+    </div>
+
+    <supports/>
+
+  </div>
+</template>
+
+<script>
+import Supports from '@/components/Supports.vue'
+
+export default {
+  name: "SupportsLessons",
+
+  components: {
+    Supports
+  }
+};
+</script>
