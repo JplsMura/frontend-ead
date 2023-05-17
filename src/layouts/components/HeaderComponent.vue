0f5c8419508f9e316f6ca02a2595ac53a90af5a2
JplsMura <joaopedrolima.s@hotmail.com>
2023-05-17 16:53:00 -0300

    Configuration router-view to campus.home Header application

--- src/layouts/components/HeaderComponent.vue
+++ src/layouts/components/HeaderComponent.vue
@@ -1,17 +1,22 @@
 <template>
-    <header id="header">
-        <nav>
-            <span class="toggleMenu far fa-bars"></span>
-            <span class="logo">
-                <img style="max-width: 50px; max-height: 50px;" :src="require('@/assets/images/logo-custom.png')" alt="EAD Plataforma">
-            </span>
-        </nav>
-    </header>
+  <header id="header">
+    <nav>
+      <span class="toggleMenu far fa-bars"></span>
+      <span class="logo">
+        <router-link :to="{ name: 'campus.home' }">
+          <img
+            style="max-width: 50px; max-height: 50px"
+            :src="require('@/assets/images/logo-custom.png')"
+            alt="EAD Plataforma"
+          />
+        </router-link>
+      </span>
+    </nav>
+  </header>
 </template>
 
 <script>
-
 export default {
-    name: 'header-component'
-}
+  name: "header-component",
+};
 </script>
