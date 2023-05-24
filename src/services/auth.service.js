418686a55508e7a5f522f9a78715b66fe4d5bf70
JplsMura <joaopedrolima.s@hotmail.com>
2023-05-24 15:45:45 -0300

    Working with Promise, to customize the return and error handling or response.

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,7 +1,12 @@
-import BaseService from "./BaseService";
+import BaseService from "./base.service";
 
 export default class AuthService extends BaseService {
     static async auth (params) {
-        return this.request().post('/auth', params)
+        return new Promise((resolve, reject) => {
+            this.request()
+                .post('/auth', params)
+                .then(response => resolve(response))
+                .catch(error => reject(error.response))    
+        })
     }
 }
