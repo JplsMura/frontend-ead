a7bd979b1b8c5c3c161852c973f6dabce78c7cb5
JplsMura <joaopedrolima.s@hotmail.com>
2023-06-10 13:39:57 -0300

    Adjustment in the Header to add the Side Menu to the application and creating the pagination component for the application's doubts

--- src/layouts/components/HeaderComponent.vue
+++ src/layouts/components/HeaderComponent.vue
@@ -1,18 +1,42 @@
 <template>
   <header id="header">
     <nav>
-      <span class="toggleMenu far fa-bars"></span>
+      <span class="toggleMenu far fa-bars" @click="toggleMenu"></span>
       <span class="logo">
         <router-link :to="{ name: 'campus.home' }">
-          <img :src="require('@/assets/images/logo_transparent.png')" alt="CodeFlow"/>
+          <img
+            :src="require('@/assets/images/logo_transparent.png')"
+            alt="CodeFlow"
+          />
         </router-link>
       </span>
     </nav>
   </header>
+
+  <menu-component v-if="showMenu"> </menu-component>
 </template>
 
 <script>
+import { ref } from "vue";
+
+import MenuComponent from "./MenuComponent.vue";
+
 export default {
   name: "header-component",
+
+  setup() {
+    const showMenu = ref(false);
+
+    const toggleMenu = () => (showMenu.value = !showMenu.value);
+
+    return {
+      showMenu,
+      toggleMenu,
+    };
+  },
+
+  components: {
+    MenuComponent,
+  },
 };
 </script>
