2b18d0b0acc9f12e55a30bb6a23e302b217e19fe
JplsMura <joaopedrolima.s@hotmail.com>
2023-06-10 13:45:05 -0300

    Adding to store/user actions of:
    Authenticate,
    getMe,
    Logout.

--- src/store/modules/users/index.js
+++ src/store/modules/users/index.js
@@ -1,39 +1,53 @@
-import AuthService from "@/services/auth.service"
-import ResetPasswordService from "@/services/password.reset.service"
+import AuthService from "@/services/auth.service";
+import ResetPasswordService from "@/services/password.reset.service";
 
 export default {
-    state: {
-        user: {
-            name: '',
-            email: '',
-        },
-        loggedIn: false,
+  state: {
+    user: {
+      name: "",
+      email: "",
+    },
+    loggedIn: false,
+  },
 
+  mutations: {
+    SET_USER(state, user) {
+      state.user = user;
+      state.loggedIn = true;
+    },
+    LOGOUT(state) {
+      state.user = {
+        name: "",
+        email: "",
+      };
+
+      state.loggedIn = false;
+    },
+  },
+
+  actions: {
+    auth({ dispatch }, params) {
+      return AuthService.auth(params).then(() => dispatch("getMe"));
     },
 
-    mutations: {
-        SET_USER (state, user) {
-            state.user = user,
-            state.loggedIn = true
-        },
+    getMe({ commit }) {
+      commit("CHANGE_LOADING", true);
 
-        LOGOUT (state) {
-            state.user = {
-                name: '',
-                email: '',
-            }
-            state.loggedIn = false
-        },
+      AuthService.getMe()
+        .then((user) => commit("SET_USER", user))
+        .finally(() => commit("CHANGE_LOADING", false));
     },
 
-    actions: {
-        auth ({state}, params) {
-            state.loggedIn
-            return AuthService.auth(params)
-        },
+    logout({ commit }) {
+      commit("CHANGE_LOADING", true);
 
-        forgetPassword (_, params) {
-            return ResetPasswordService.forgetPassword(params)
-        },
+      return AuthService.logout()
+        .then(() => commit("LOGOUT"))
+        .finally(() => commit("CHANGE_LOADING", false));
     },
-}
+
+    forgetPassword(_, params) {
+      return ResetPasswordService.forgetPassword(params);
+    },
+  },
+};
