a86e88bb212b7e896302c9be90e5eb7b70040bc8
JplsMura <joaopedrolima.s@hotmail.com>
2023-06-10 13:37:02 -0300

    Routes, adjustments and implementation of beferoEach for validation in 'Reset Password' and 'getMe'.

--- src/router/index.js
+++ src/router/index.js
@@ -1,62 +1,79 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Home from "../views/home/Home.vue";
-import MySupports from "../views/supports/MySupports.vue";
-import ModulesAndLessons from "../views/modules/ModulesAndLessons.vue";
-import Auth from "../views/auth/Auth.vue";
-import ForgetPassword from "../views/auth/ForgetPassword.vue";
-import ResetPassword from "../views/auth/ResetPassword.vue";
+import { createRouter, createWebHistory } from "vue-router";
+import Home from "@/views/home/Home.vue";
+import MySupports from "@/views/supports/MySupports.vue";
+import ModulesAndLessons from "@/views/modules/ModulesAndLessons.vue";
+import Auth from "@/views/auth/Auth.vue";
+import ForgetPassword from "@/views/auth/ForgetPassword.vue";
+import ResetPassword from "@/views/auth/ResetPassword.vue";
 
+import store from "@/store";
+import { TOKEN_NAME } from "@/configs";
 
 const routes = [
-  /* Component relacionados  a módulos, aulas e suportes na tela de aulas */
   {
-    path: '/campus',
-    component: () => import('@/layouts/DefaultTemplate.vue'),
+    path: "/campus",
+    component: () => import("@/layouts/DefaultTemplate.vue"),
     children: [
       {
-        path: 'modulos',
-        name: 'campus.modulos',
+        path: "modulos",
+        name: "campus.modulos",
         component: ModulesAndLessons,
-      }, {
-        path: 'minhas-duvidas',
-        name: 'campus.my.supports',
+      },
+      {
+        path: "minhas-duvidas",
+        name: "campus.my.supports",
         component: MySupports,
-      }, {
-        path: '',
-        name: 'campus.home',
+      },
+      {
+        path: "",
+        name: "campus.home",
         component: Home,
       },
     ],
   },
-
-  /* Component relacionados a tela de login, recuperar senha e esqueceu a senha */
   {
     path: '/',
     component: () => import('@/layouts/components/auth/TemplateAuth.vue'),
     children: [
       {
-        path: '/',
-        name: 'auth',
+        path: "/",
+        name: "auth",
         component: Auth,
       },
       {
-        path: '/recuperar-senha',
-        name: 'forget.password',
+        path: "/recuperar-senha",
+        name: "forget.password",
         component: ForgetPassword,
       },
       {
-        path: '/reset/:token',
-        name: 'reset.password',
+        path: "/reset/:token",
+        name: "reset.password",
         component: ResetPassword,
         props: true,
       },
-    ],
+    ]
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
+
+router.beforeEach(async (to, _, next) => {
+  const loggedIn = store.state.users.loggedIn;
+  if (to.name != "reset.password" && !loggedIn) {
+    const token = await localStorage.getItem(TOKEN_NAME);
+    if (!token && to.name != "auth" && to.name != "forget.password") {
+      return router.push({ name: "auth" });
+    }
+
+    await store.dispatch("getMe").catch(() => {
+      if (to.name != "auth") return router.push({ name: "auth" });
+    });
+  }
+
+  next();
+});
 
 export default router;
